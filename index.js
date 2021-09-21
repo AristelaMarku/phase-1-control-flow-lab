403542ef3857d77eafc185d7e8eb1fdeaa63634b
Aristela Marku <aristela.marku@gmail.com>
2021-09-21 11:05:19 +0200

    commit tree d57c30ae0c2538f3d0dc50283a302047a221ea78

--- index.js
+++ index.js
@@ -1,11 +1,23 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(feet){
+  if (feet <= 400) return "This one is on me!";
+  else if (feet < 2500 && feet > 2000) return "I will gladly take your thirty bucks.";
+  else if (feet > 2500) return "No can do.";
+}
+function ternaryCheckCity(city){
+  let result;
+  result=city==="NYC"?"Ok, sounds good.":"No go."
+  return result
+// Write your code here!
 }
 
-function ternaryCheckCity(){
+function switchOnCharmFromTip(tips){
   // Write your code here!
-}
-
-function switchOnCharmFromTip(){
-  // Write your code here!
+  switch(tips){
+    case 'generous':
+      return "Thank you so much."
+    case 'not as generous':
+       return 'Thank you.'
+      default:
+        return 'Bye.'
+  }
 }
